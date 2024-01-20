b6e7a5cea30d2f680dc43d2de844a74c45a68ac0
MintChocolateMan <eliasmanis05@gmail.com>
2024-01-19 18:56:57 -0600

    g

--- Little_Boy/src/main/java/frc/robot/robotCode/ConstantsAndConfigs/Constants.java
+++ Little_Boy/src/main/java/frc/robot/robotCode/ConstantsAndConfigs/Constants.java
@@ -2,4 +2,10 @@ package frc.robot.robotCode.ConstantsAndConfigs;
 
 public class Constants {
     
+    public static class DrivetrainConstants {
+        public static int frontLeftMotorPort = 5;
+        public static int rearLeftMotorPort = 2;
+        public static int frontRightMotorPort = 3;
+        public static int rearRightMotorPort = 4;
+    }
 }
